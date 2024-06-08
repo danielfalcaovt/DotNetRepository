11f189332e5b52733dbc7ace95b23abf646114a0
danielfalcaovt <danielfalcaoviktorio@gmail.com>
2024-06-08 19:56:37 -0300

    feat: calculadora simples e estudo de array

---
+++ Arrays/Arrays/Program.cs
@@ -0,0 +1,28 @@
+﻿namespace Arrays
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.Write("Type your name: ");
+            string username = Console.ReadLine();
+            string lengthMessage = showMessage(username);
+            Console.WriteLine(lengthMessage);
+        }    
+        static string showMessage(string person)
+        {
+            if (person.Length >= 10)
+            {
+                return "Greather than 10 letters";
+            }
+            else if (person.Length == 10)
+            {
+                return "It's 10 letters";
+            }
+            else
+            {
+                return "Small than 10 letters";
+            }
+        }
+    }
+}
---
+++ CalculadoraSimples/CalculadoraSimples/Program.cs
@@ -0,0 +1,16 @@
+﻿namespace CalculadoraSimples
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.Write("The first number: ");
+            double num1 = Convert.ToDouble(Console.ReadLine());
+            Console.WriteLine();
+            Console.Write("The second number: ");
+            double num2 = Convert.ToDouble(Console.ReadLine());
+            double result = num1 + num2;
+            Console.WriteLine($"Your result is {result}");
+        }
+    }
+}
